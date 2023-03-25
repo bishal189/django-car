b2c70f21607a529f1650724bb1d930289fd6ed4e
bishal189 <bishal@gmail.com>
2023-03-25 18:13:46 +0545

    deployment

--- carzone/settings.py
+++ carzone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e*=&(w%ugs60%-$5i-2g=_2d^9hpgn)93x12r-jqf0i#m&&t+o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
